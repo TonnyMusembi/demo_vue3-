fd68b2dd1a47b1f42e8f6af67332ce0121cb655c
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-12 13:36:59 +0300

    add

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 // import './style.css'
 import './index.css'
 import App from './App.vue'
+import store from "./store";
+
 import router from "./router";
+
 import { RouterLink, RouterView } from "vue-router";
+
+
 const app = createApp(App);
 app.use(router);
-// app.use(store);
+app.use(store);
 app.use(RouterLink);
 app.mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import auth from "./auth";
+// import auth from "./auth";
 export default createStore({
-    modules: { auth },
+    // modules: { auth },
 });
