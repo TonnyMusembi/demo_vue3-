f594eb735b437e96bde641a64c00f21957f35ef6
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-14 09:53:45 +0300

    add

--- src/views/odd.vue
+++ src/views/odd.vue
@@ -75,7 +75,7 @@ onMounted(() => {
     <button
       class="bg-green-800 text-white active:bg-green-800 font-bold uppercase text-sm px-1 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
     >
-      Odds
+      Teams
     </button>
   </div>
    <div class="relative p-2">
