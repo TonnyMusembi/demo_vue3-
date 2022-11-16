78b0c7e30d5ee352b8c60694b0112aa6c9f1980d
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-16 16:00:42 +0300

    add

---
+++ src/views/login_user.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container mx-auto w-1/2 bg-gray-100">
+    <div class="flex justify-center h-full">
+      <div class="h-full items-center p-8 pt-48">
+        <div class="w-full">
+          <div class="block">
+            <form @submit.prevent="">
+              <div class="text-xl mb-2 text-black-500 font-medium">Login</div>
+
+              <div class="block mb-3">
+                <span>
+                  <label
+                    for=""
+                    class="block text-xs font-bold text-gray-500 mb-2"
+                    >Phone number</label
+                  >
+                </span>
+                <input
+                  name="Phone Number"
+                  class="block rounded-md w-full py-2 px-3 border outline-none focus-visible:border-cyan-400"
+                  maxlength="12"
+                  placeholder="07xxxxxxxx"
+                  required
+                />
+              </div>
+              <div class="block mb-3">
+                <span>
+                  <label
+                    for=""
+                    class="block text-xs font-bold text-gray-500 mb-2"
+                    >Password</label
+                  >
+                </span>
+                <input
+                  name="Password"
+                  class="block rounded-md w-full py-2 px-3 border outline-none focus-visible:border-cyan-400"
+                  type="password"
+                  required
+                />
+              </div>
+              <div
+                class="block px-4 py-2 text-gray-600 text-sm rounded-md"
+              ></div>
+              <div class="block">
+                <button
+                  type="submit"
+                  class="block w-full text-center rounded-md bg-cyan-400 px-3 py-2 text-white"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="my-4">
+            <router-link :to="{ name: '' }" class="my-4 text-sm underline"
+              >Forgot Password</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
