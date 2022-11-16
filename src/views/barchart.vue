0c34645f69657effbb93818c86d52c3433900915
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-16 10:35:34 +0300

    add

--- src/views/barchart.vue
+++ src/views/barchart.vue
@@ -54,7 +54,7 @@ onMounted(() => {
             />
           </svg>
         </div>
-        <div class="text-sm font-medium">No of loansa Disbursed</div>
+        <div class="text-sm font-medium">Registered Companies</div>
       </div>
       <div
         class="col shadow-md rounded-md w-full py-1 px-2 bg-white-700 text-black"
@@ -81,16 +81,22 @@ onMounted(() => {
         class="col shadow-md rounded-md w-full py-1 px-2 bg-white-700 text-black"
       >
         <div>
-          <svg width="32" height="32" viewBox="0 0 16 16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            class="w-6 h-6"
+            viewBox="0 0 24 24"
+          >
             <path
-              fill="currentColor"
-              fill-rule="evenodd"
-              d="M14.5 2H9l-.35.15-.65.64-.65-.64L7 2H1.5l-.5.5v10l.5.5h5.29l.86.85h.7l.86-.85h5.29l.5-.5v-10l-.5-.5zm-7 10.32-.18-.17L7 12H2V3h4.79l.74.74-.03 8.58zM14 12H9l-.35.15-.14.13V3.7l.7-.7H14v9zM6 5H3v1h3V5zm0 4H3v1h3V9zM3 7h3v1H3V7zm10-2h-3v1h3V5zm-3 2h3v1h-3V7zm0 2h3v1h-3V9z"
-              clip-rule="evenodd"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
             />
           </svg>
         </div>
-        <div class="text-sm font-medium">Registered Companies</div>
+        <div class="text-sm font-medium">No of loans Disbursed</div>
       </div>
       <div
         class="col shadow-md rounded-md w-full py-2 px-2 bg-white-700 text-black"
